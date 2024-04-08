adeb08bd90177b6de68ae98a009d8cee818228ed
apoorv <apoorv654123@gmail.com>
2024-04-08 18:25:43 +0530

    added logo and icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import logo from '../assets/TaskComp.png'
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between bg-blue-800 text-white'>
-      <div className="logo">
-        <span className="font-bold text-xl mx-9">TaskComp</span>
+      <div className="logo flex">
+        <img src={logo} alt="logo" className='ml-9 mr-3 w-7' />
+        <span className="font-bold text-xl mr-9">TaskComp</span>
       </div>
       <ul className='flex gap-8 mx-9'>
         <li className="cursor-pointer hover:font-bold transition-all duration-200">Home</li>
